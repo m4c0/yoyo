4020e22c8f717baca8948aceb11148de919c1597
Eduardo Costa <m4c0@users.noreply.github.com>
2022-12-19 16:00:14 +0000

    split build for import by other modules

--- build.cpp
+++ build.cpp
@@ -1,18 +1,12 @@
-#include "ecow.hpp"
+#include "build.hpp"
 
 using namespace ecow;
 
 int main(int argc, char **argv) {
   seq all{"all"};
 
-  auto m = all.add_unit<mod>("yoyo");
-  m->add_part("reader");
-  m->add_part("writer");
-  m->add_part("ce_reader");
-  m->add_part("istr_reader");
-  m->add_part("ostr_writer");
-  m->add_part("subreader");
-  m->add_impl("ce_reader-test");
+  auto m = yoyo();
+  all.add_ref(m);
 
   auto tst = all.add_unit<exe>("tests");
   tst->add_unit<>("subreader-test");
---
+++ build.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ecow.hpp"
+
+auto yoyo() {
+  using namespace ecow;
+  auto m = unit::create<mod>("yoyo");
+  m->add_part("reader");
+  m->add_part("writer");
+  m->add_part("ce_reader");
+  m->add_part("istr_reader");
+  m->add_part("ostr_writer");
+  m->add_part("subreader");
+  m->add_impl("ce_reader-test");
+  return m;
+}

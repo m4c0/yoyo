2a5c75de5e1cde85c49d885ef1e1fd4780f16ded
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-28 11:05:38 +0100

    removing unmaintained "yoyo_stl"

--- build.hpp
+++ build.hpp
@@ -7,11 +7,6 @@ auto yoyo() {
   return unit::create<box>("yoyo");
 }
 
-auto yoyo_stl() {
-  using namespace ecow;
-  return unit::create<box>("yoyo_stl");
-}
-
 auto yoyo_libc() {
   using namespace ecow;
   return unit::create<box>("yoyo_libc");
--- yoyo_stl.cppm
+++
@@ -1,4 +0,0 @@
-export module yoyo_stl;
-export import :istr_reader;
-export import :ostream_writer;
-export import :span_reader;
